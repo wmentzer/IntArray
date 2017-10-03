afa6168eeeb4a6e3a6a6912ee461073c652d020c
William <willmentzer20@gmail.com>
2017-10-02 19:32:58 -0700

    Finished IntArray Program

--- main.cpp
+++ main.cpp
@@ -5,61 +5,53 @@
 using namespace std;
 
 class IntArray {
-public:
-	IntArray(int size);
-	~IntArray();
+public:					// "Public" allows everything written
+	IntArray(int size);		// within the section to be accessed
+	~IntArray();			// by outside sources
 	int & operator[](int i) const;
 	int size() const;
 	void fill(int k);
 	int * begin() {
-	return data[0];
-};
+		return data;
+	};
 	int * end() {
-	return data[len];
-};
-private:
-	int len;
+		return (data + len);
+	};
+private:		// "Private" makes code only accessable within
+	int len;	// the IntArray class
 	int * data;
 };
 
-IntArray::IntArray(int size) {
-	len = size;
-	new int[size] data;
+IntArray::IntArray(int size) {	// Constructor - allocates memory for
+	len = size;		// the IntArray stored as "data"
+	data = new int[len];
 };
 
-IntArray::~IntArray() {
-	delete [] data;
+IntArray::~IntArray() {		// Destructor - deletes the allocated
+	delete [] data;		// memory stored for the IntArray
 };
 
-int & IntArray::operator[](int i) const {
-	return data[i];
+int & IntArray::operator[](int i) const {	// Defines the operator[] to allow
+	return data[i];				// returning of specific data values
 };
 
-int IntArray::size() const {
-	return len;
+int IntArray::size() const {	// Returns the given length
+	return len;		// or size of the IntArray
 };
 
-void IntArray::fill(int k) {
-	for(int i = 0; i < len; i++) {
+void IntArray::fill(int k) {		// Fills all the slots in data with
+	for(int i = 0; i < len; i++) {	// the given value of "k"
 		data[i] = k;
 	}
 };
 
-/*int IntArray::*begin() {
-	return data[0];
-}
-
-int IntArray::*end() {
-	return data[len];
-}
-*/
 int main(int argc, char * args[]) {
 	IntArray a(3);
 	a[0] = 10;
 	a[1] = 11;
         a[2] = 12;
-        assert(a[0] == 10);
-        assert(a[1] == 11);
+        assert(a[0] == 10);	// assert() is used to check if the given
+        assert(a[1] == 11);	// statement is true without printing to the screen
         assert(a[2] == 12);
         assert(a.size() == 3);
         a.fill(5);
